b3e86abae1b280da1c14d6e20ae6278cffe3716d
Raisa Thomas <raisat316@gmail.com>
2021-06-17 15:23:05 +0530

    First Commit

---
+++ src/Myfirstcode.java
@@ -0,0 +1,12 @@
+
+public class Myfirstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 30 , b = 30 , c ;
+			c = a+b ;
+			System.out.println("Results :" + c);
+			System.out.println("Thankyou");
+	}
+
+}
